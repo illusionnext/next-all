1866e0473deab3967be4a43fefa80c56056c14d7
Farid <fargan@st.amu.edu.pl>
2025-03-11 03:33:24 +0100

    ➕crossOrigin: "anonymous",                   // development: {                   //   appDir: true,                   //   runtime: "edge"                   // },                   reactStrictMode: true,
                      transpilePackages: ["three"], // transpile three.js
    
                      experimental: {
                        authInterrupts: true, // forbidden, unauthorized, and expired sessions
                        reactCompiler: true,
    
                        cssChunking: true,
                        inlineCss: true,
                        useLightningcss: true,
                        viewTransition: true,
    
                        ppr: "incremental",
                        dynamicIO: true,
                        useCache: true,
                        staleTimes: {
                          dynamic: 30,
                          static: 180,
                        },
    
                        staticGenerationRetryCount: 1,
                        staticGenerationMaxConcurrency: 8,
                        staticGenerationMinPagesPerWorker: 25,
    
                        typedRoutes: true,
    
                        urlImports: ["https://example.com/assets/", "https://cdn.skypack.dev"],
    
                        webVitalsAttribution: ["LCP", "FID", "CLS", "TTFB", "FCP", "INP"],
                      },

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,42 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  crossOrigin: "anonymous",
+  // development: {
+  //   appDir: true,
+  //   runtime: "edge"
+  // },
+  reactStrictMode: true,
+
+  transpilePackages: ["three"], // transpile three.js
+
+  experimental: {
+    authInterrupts: true, // forbidden, unauthorized, and expired sessions
+    reactCompiler: true,
+
+    cssChunking: true,
+    inlineCss: true,
+    useLightningcss: true,
+    viewTransition: true,
+
+    ppr: "incremental",
+    dynamicIO: true,
+    useCache: true,
+    staleTimes: {
+      dynamic: 30,
+      static: 180,
+    },
+
+    staticGenerationRetryCount: 1,
+    staticGenerationMaxConcurrency: 8,
+    staticGenerationMinPagesPerWorker: 25,
+
+    typedRoutes: true,
+
+    urlImports: ["https://example.com/assets/", "https://cdn.skypack.dev"],
+
+    webVitalsAttribution: ["LCP", "FID", "CLS", "TTFB", "FCP", "INP"],
+  },
 };
 
 export default nextConfig;
